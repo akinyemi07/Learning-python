bd4f03f76af7c809458df3a742dd8136e67f763d
@akinyemi07 <soldinixpresini@gmail.com>
2024-08-07 10:23:32 +0100

    work in progress

---
+++ Answers/number1.py
@@ -0,0 +1,9 @@
+print("use for, split(), and if, to create a statement that will print out the letters of the string that starts with 's'")
+
+print("st ='print only the words that starts with 's' in the statement'")
+
+
+print("solution")
+
+
+ 
