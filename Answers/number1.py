78dce8b0f1a8c0d7f1b122209e8a902a379cf1d5
@akinyemi07 <soldinixpresini@gmail.com>
2024-08-08 08:49:23 +0100

    number one complete and running

--- Answers/number1.py
+++ Answers/number1.py
@@ -5,5 +5,13 @@ print("st ='print only the words that starts with 's' in the statement'")
 
 print("solution")
 
+st="print only the words that starts with 's' in the statment"
 
- 
+y=st.split()
+
+for word in y:
+    if word[0] =='s'
+        print (word)
+
+input('please press enter; ')
+        
