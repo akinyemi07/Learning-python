8ff7900b10b6b48a1abd4119296a1f1736306cf5
@akinyemi07 <soldinixpresini@gmail.com>
2024-08-08 15:04:02 +0100

    initial commit, we begin

---
+++ Answers/number2.py
@@ -0,0 +1,9 @@
+print("question 2: Use range() to print all even numbers from 1 to 100")
+
+print("Solution")
+
+x= range(1,100)
+
+for num in x:
+    if num % 2==0:
+        print ("the number {num} is an even number")
