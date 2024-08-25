db85c86dc2377a4fd4940b55bf37b4b9e7e1326c
@akinyemi07 <soldinixpresini@gmail.com>
2024-08-25 19:40:52 +0100

    this is not completed i just have to do something quickly

---
+++ Answers/ques3.py
@@ -0,0 +1,5 @@
+#a python progra, to use list comprehension to create a list of all numbes between 1 and 50 that are divisible by 3
+
+x=[num for num in range(51) if num % 3 == 0]
+
+end=input("please press enter; ")
---
+++ Answers/ques4.py
@@ -0,0 +1,12 @@
+#a program -if the lenght of a word is even then print "even"
+   
+
+
+st= "print every word in this sentence that has an even number of letters"
+
+print("solution")
+
+print(st)
+
+for st:
+    if len[num] in st.split()
