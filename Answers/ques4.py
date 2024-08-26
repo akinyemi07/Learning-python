2f6719a38c7881917a0573f0603b23d25493e377
@akinyemi07 <soldinixpresini@gmail.com>
2024-08-26 16:37:10 +0100

    question number four has been solved

--- Answers/ques4.py
+++ Answers/ques4.py
@@ -1,12 +1,15 @@
 #a program -if the lenght of a word is even then print "even"
-   
-
 
 st= "print every word in this sentence that has an even number of letters"
 
 print("solution")
 
-print(st)
+st_Lst=st.split()
+for num in st_Lst:
+    if len(num) % 2 == 0:
+        print ("even")
+    else:
+        print(num)
 
-for st:
-    if len[num] in st.split()
+end = "please press enter ; "
+
