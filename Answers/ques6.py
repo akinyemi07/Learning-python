3b0dc7dc7b55642eef72d3194a324988d768a00d
@akinyemi07 <soldinixpresini@gmail.com>
2024-08-27 15:18:51 +0100

    number six done

--- Answers/ques6.py
+++ Answers/ques6.py
@@ -1,2 +1,10 @@
 #a program to use list compreshension to write
 st= "create a list of the first letters of every word in the string"
+
+
+x=[word[0] for item in st.split()]
+
+print(x)
+
+
+end = "please press enter ; "
